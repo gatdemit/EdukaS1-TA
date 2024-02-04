709231b745acccb41b73393a80fbd0279e4cb5b9
gatdemit <muhriz14@gmail.com>
2024-02-05 06:36:08 +0700

    5 Februari 2024 minor

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-between">
     <div>
-      <a href="/" class="nav-link fw-bold ff-inter" style="font-size: 34px;">Edukas<span style="color: #0038CF">1</span></a>
+      <a href="/" class="nav-link" style="font-size: 34px;"><img src="{{ asset('storage/asset/EDUKAS1_1.png') }}" style="max-height:64px; max-width:128px;"></a>
     </div>
     <div>
         @include("partials.navbar")
