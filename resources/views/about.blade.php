bd9291ffdf5c39cb384d34d6fa68ffb88605eb90
Alvin Arrazy <alvin.arrazy@shopee.com>
2024-02-07 14:32:52 +0700

    fix: revisi 5.3, about page layouts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.main')
 
 @section('container')
+<div class="container">
     <div class="row mt-5">
-        <div class="col-5 offset-1" style="margin-right: 40px;">
+        <div class="col" style="margin-right: 40px; display: flex; flex-direction: column; justify-content: center">
             <h1 class="text-primary" style="font-weight:600; font-size: 36px;">Tentang Edukas1</h1>
             <p>Kami di Edukas1 berkomitmen untuk memberikan pendidikan berkualitas tinggi dan aksesibel kepada semua orang. Kami percaya bahwa pendidikan adalah kunci untuk pertumbuhan dan kemajuan pribadi, dan kami berdedikasi untuk membantu mahasiswa mencapai potensi terbaik mereka.</p>
             <p>Edukas1 adalah sebuah platform pendidikan daring yang berfokus pada penyediaan beragam kursus mata kuliah untuk mahasiswa. Kami adalah tim berdedikasi yang terdiri dari pengajar berkualitas, desainer instruksional, dan pengembang web yang bersemangat tentang memberikan pengalaman belajar terbaik kepada pengguna kami.</p>
         </div>
-        <div class="col-5">
-            <img src="{{ asset('storage/asset/About_1.jpeg') }}" alt="" style="width: 500px; height: 500px;">
+        <div class="col">
+            <img src="{{ asset('storage/asset/About_1.jpeg') }}" alt="" style="width: 100%; height: 100%;">
         </div>
     </div>
     <div class="row mt-5">
-        <div class="col-5 offset-1" style="margin-right: 40px;">
-            <img src="{{ asset('storage/asset/About_2.jpeg') }}" alt="" style="width: 500px; height: 500px;">
+        <div class="col" style="margin-right: 40px;">
+            <img src="{{ asset('storage/asset/About_2.jpeg') }}" alt="" style="width: 100%; height: 100%;">
         </div>
-        <div class="col-5">
+        <div class="col" style="display: flex; flex-direction: column; justify-content: center">
             <h1 class="text-primary" style="font-weight:600; font-size: 36px;">Apa Yang Kami Tawarkan?</h1>
             <p>Kami menawarkan beragam kursus dari berbagai mata kuliah yang diajarkan oleh instruktur berpengalaman. Setiap kursus dirancang dengan cermat untuk memastikan pengalaman belajar yang efektif dan menarik. Di sini, Anda dapat mengakses:</p>
             <ul>
@@ -25,4 +26,5 @@
             </ul>
         </div>
     </div>
+</div>
 @endsection
