d641cfe6b29999919d8c144509179678954c24a6
alvinarrazy <alvinarrazy29@gmail.com>
2024-02-11 12:30:34 +0700

    fix: revisi 5.13, laporan layouts

--- resources/views/adPanel/sidemenu/laporan/index.blade.php
+++ resources/views/adPanel/sidemenu/laporan/index.blade.php
@@ -63,13 +63,24 @@
                 <label for="bulan">Bulan</label>
             </div>
         </form>
-        <div class="row mb-4">
-            <div class="col mr-4">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Tahunan</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Bulanan</button>
+            </li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
                 <div class="card">
                     <canvas id="barChart"></canvas>
                 </div>
             </div>
-            <div class="col mr-4">
+            <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
                 <div class="card">
                     <canvas id="barBulan"></canvas>
                 </div>
